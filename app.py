6e490319e489ffc77118893b3f63c07020427327
leab38 <43719429+leab38@users.noreply.github.com>
2022-10-12 10:12:40 -0500

    Add form fields and test submit logic

--- app.py
+++ app.py
@@ -1,7 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/form')
+def form():
+    return render_template('index.html')
+
+@app.route('/data/', methods = ['POST','GET'])
+def data():
+    if request.method == 'GET':
+        return f"You are trying to access /data directly, try going to /form instead to submit form."
+    if request.method == 'POST':
+        form_data = request.form
+        return render_template('data.html',form_data = form_data)
